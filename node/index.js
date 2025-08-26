9099c790c49c207e9504ad0159d36846ae088a2e
coshinehcl <hcl@coshinehcldeMacBook-Pro.local>
2025-08-26 11:10:04 +0800

    fix: 更新物料

--- node/index.js
+++ node/index.js
@@ -37,6 +37,17 @@ const server = https.createServer(options,(req, res) => {
                 res.end(content);
             }
         });
+    } else if(urlPath ==='/home.html') {
+        const filePath = path.join(__dirname, `.${urlPath}`);
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('File not found');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(content);
+            }
+        });
     } else {
         // 对其他路由返回404
         res.writeHead(404, {'Content-Type': 'text/plain'});
